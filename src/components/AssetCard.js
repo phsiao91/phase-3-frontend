baa037f683cd7d6cf05358f19f78c81db58b3fd1
Michael Burt <burtrico@gmail.com>
2021-09-16 00:19:58 -0500

    fetch crypto

---
+++ src/components/AssetCard.js
@@ -0,0 +1,73 @@
+import React, {useState} from 'react'
+
+
+function AssetCard({ asset }) {
+
+    // const [ history, setHistory ] = useState( [] )
+
+
+    // const handleClick = () => {
+    //     console.log(place.name)
+    //     selectPlace(place)
+    //     postHistory(place)
+    // }
+
+    
+
+    // function postHistory(newHis){
+    //     fetch('', {
+    //       method: 'POST',
+    //       headers: {
+    //         'Content-Type': 'application/json'
+    //       },
+    //       body: JSON.stringify(newHis)
+    //     })
+    //     .then (response => response.json())
+    //     .then(oneHis => { console.log(oneHis)
+        
+    //      setHistory([ ...history, oneHis ])
+    //     })
+        
+    //   }
+    
+
+
+  // const price = tokenArray.market_data.current_price.usd
+  // tokenPrice.innerHTML = `<u>Current Price:</u> <span class"=bold">$${price.toLocaleString('en-US')}</span>`;
+
+  // const priceChange24h = tokenArray.market_data.price_change_percentage_24h
+  // if (priceChange24h > 0) {
+  //   tokenPriceChange24h.innerHTML = `<u>Price Change 24h:</u> <span class="fontGreen">%${priceChange24h}</span>`;
+  // } else { tokenPriceChange24h.innerHTML = `<u>Price Change 24h:</u> <span class="fontRed">%${priceChange24h}</span>`;
+  // }
+
+  // const marketCap = tokenArray.market_data.market_cap.usd
+  // tokenMarketCap.innerHTML = `<u>Market Cap:</u> $${marketCap.toLocaleString('en-US')}`;
+
+  // const marketCapRank = tokenArray.market_cap_rank
+  // tokenMarketCapRank.innerHTML = `<u>Market Cap Rank:</u> ${marketCapRank}`;
+
+  // const totalSupply = tokenArray.market_data.total_supply
+  // tokenCirculatingSupply.innerHTML = `<u>Circulating Supply:</u> ${circulatingSupply.toLocaleString('en-US')}`;
+
+  // const descriptionCG = tokenArray.description.en;
+  // tokenDescription.innerHTML = `<u>Description:</u> ${descriptionCG}`;
+
+  // const imgLarge = tokenArray.image.large
+
+
+    console.log("assetCard:", asset)
+
+    return(
+        <div className="card">
+            <h2 className="asset">{asset.name} ({asset.symbol}):: Bought at ${asset.price}</h2>
+            <li className="card">
+                {/* <button className="buttonImg" onClick={handleClick}>
+                </button> */}
+                <p></p>
+            </li>
+        </div>
+    )
+}
+
+export default AssetCard;
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PortfolioTab from './PortfolioTab';
+import AssetCard from './AssetCard';
 import NavBar from './NavBar';
 import { useEffect, useState} from 'react'
 
@@ -21,6 +22,50 @@ function Portfolio (props) {
     })
     }
     useEffect(getAssets, [])
+
+
+    // FETCH Crypto Assets
+
+  const [ cryptoAssets, setCryptoAssets ] = useState( [] )
+
+
+  const getMarketAssets = () => {
+
+    const cryptoAssets = ["ethereum", "bitcoin", "aave", "bankless-dao"]
+    const allTokensArray = [];
+    
+    cryptoAssets.forEach(asset => { 
+      console.log(asset)
+
+      // place ${tokenObj.name} into URL below in order to fetch portfolio assets
+      fetch(`https://api.coingecko.com/api/v3/coins/${asset}`, {method: 'GET'})
+      .then(response => response.json())
+      .then(tokenObj => {
+            console.log(tokenObj.name)
+            const localObj = {
+            "id": tokenObj.id,
+            "name": tokenObj.name, 
+            "symbol": tokenObj.symbol,
+            "price": tokenObj.market_data.current_price.usd,
+            "market_cap": tokenObj.market_data.market_cap.usd,
+            "price_24h": tokenObj.market_data.price_change_percentage_24h,
+            "market_cap_rank": tokenObj.market_cap_rank,
+            "total_supply": tokenObj.market_data.total_supply,
+            "image": tokenObj.image.large
+            }
+            
+            allTokensArray.push(localObj) 
+
+          })                         
+      })
+      setCryptoAssets(allTokensArray) 
+  }
+
+  useEffect(getMarketAssets, [] )
+
+
+
+
     
     const mapAssets = () => {
         let mappedAssets = assets.map(eachAsset =>{
@@ -33,14 +78,35 @@ function Portfolio (props) {
         return mappedAssets
     }
 
+    const mapMarketAssets = () => {
+        let mappedCryptoAssets = cryptoAssets.map(eachAsset =>{
+            console.log(eachAsset.symbol)
+            return(
+                <AssetCard key={eachAsset.id}
+                    asset={eachAsset}
+                />
+            )
+        })
+        return mappedCryptoAssets
+    }
+
 
 
 
     return(
-        <div>
+        <div id="page">
              <NavBar/>
-             <h2 className="welcome">Start Investing {props.renderUser}!!!</h2>
+
+             <div id="divMarket">
+             <h2 className="welcome">The Market</h2>
+             {mapMarketAssets()}
+             </div>
+
+             <div id="divPortfolio">
+             <h2 className="welcome">{props.renderUser}'s Portfolio</h2>
              {mapAssets()}
+
+             </div>
             
         </div>
     )
